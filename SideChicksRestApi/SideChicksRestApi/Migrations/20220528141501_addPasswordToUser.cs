fddb892fef8bfd757556e3881d9314a6543a5b6f
Shabular <43166019+Shabular@users.noreply.github.com>
2022-05-28 16:21:36 +0200

    adding migrations

---
+++ SideChicksRestApi/SideChicksRestApi/Migrations/20220528141501_addPasswordToUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SideChicksRestApi.Migrations
+{
+    public partial class addPasswordToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+        }
+    }
+}
