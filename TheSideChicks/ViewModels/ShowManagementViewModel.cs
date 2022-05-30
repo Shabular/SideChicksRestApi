e716e75214e8e395106b746e01f5bfd371334eb0
Shabular <43166019+Shabular@users.noreply.github.com>
2022-05-30 20:27:35 +0200

    working

---
+++ TheSideChicks/ViewModels/ShowManagementViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheSideChicks.Services;
+using TheSideChicks.View;
+
+namespace TheSideChicks.ViewModels
+{
+    [QueryProperty("ShowsViewModel", "ShowsViewModel")]
+    public partial class ShowManagementViewModel : BaseViewModel
+    {
+        [ObservableProperty]
+        ShowsViewModel showsViewModel;
+
+        LocationService locationService;
+        ShowService showService;
+        
+
+
+        public ShowManagementViewModel(LocationService locationService,ShowService showService)
+        {
+            Title = "Lets Go!";
+            Console.Write(showsViewModel);
+            this.locationService = locationService;
+            this.showService = showService; 
+        }
+
+        
+        
+
+
+        [ICommand]
+        async Task GoBack()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+
+        [ICommand]
+        async Task GoToShowManagement(Show show)
+        {
+            /*if (connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                
+                await Shell.Current.DisplayAlert("Internet issue", $"Check your internet and try again", "OK");
+                return;
+            }*/
+            Console.Write(show);
+            if (show is null)
+            {
+                await Shell.Current.DisplayAlert("Internet issue", $"Check your internet and try again", "OK");
+                return;
+            }
+
+
+            ShowTime showtime = new ShowTime();
+            showtime.show = show;
+            showtime.location = await locationService.GetLocationById(show.locationId);
+            showtime.location.adress = $"{showtime.location.street} {showtime.location.number}";
+            showtime.location.contactInfo = $": email: {showtime.location.email} | phone number: {showtime.location.number}";
+
+
+            await Shell.Current.GoToAsync($"{nameof(ManageBookingPage)}", true,
+
+                new Dictionary<string, object>
+                {
+                    { "ShowTime", showtime }
+                });
+        }
+        
+
+    }
+}
---
+++ TheSideChicks/ViewModels/UserViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheSideChicks.Services;
+using TheSideChicks.View;
+
+namespace TheSideChicks.ViewModels
+{
+    public partial class UserViewModel : BaseViewModel
+    {
+        UserService userService;
+        ShowService showService;
+        LocationService locationService;
+        
+
+        public ObservableCollection<Show> Shows { get; } = new();
+        public ObservableCollection<ShowsViewModel> showsViewModel { get; } = new();
+        public string username { get; set; } = Preferences.Get("username", "Not logged in");
+
+        public UserViewModel(UserService userService,ShowService showService, LocationService locationService)
+        {
+            Title = $"Welcom {username}";
+            this.userService = userService;
+            this.showService = showService;
+            this.locationService = locationService; 
+        }
+
+        [ICommand]
+        async Task NotImplemented()
+        {
+            await Shell.Current.DisplayAlert("Not implemented", $"TI am sorry to inform you that we did not implement this jet", "OK");
+            await Shell.Current.GoToAsync($"{nameof(MembersPage)}");
+        }
+        
+       
+        [ICommand]
+        async Task GetPendingShowsAsync()
+        {
+            if (IsBusy)
+                return;
+
+            try
+            {
+                IsBusy = true;
+
+                ShowsViewModel showsViewModel = new(showService, locationService);
+                var shows = await showService.GetShows();
+
+                if (Shows.Count != 0)
+                    Shows.Clear();
+
+                foreach (var show in shows)
+                    if ((show.accepted is false) & (show.date >= DateTime.Today))
+                    {
+                        showsViewModel.Shows.Add(show);
+                    }
+
+                
+                
+
+                await Shell.Current.GoToAsync(nameof(ShowManagementPage), true,
+                    new Dictionary<string, object>
+                        {
+                            { "ShowsViewModel", showsViewModel }
+                    });
+                
+            
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await Shell.Current.DisplayAlert("Error!", $"Unable to get shows: {ex.Message}", "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
+
